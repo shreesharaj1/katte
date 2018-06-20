7df89b9ad083636635dcb025f597f13405ec438d
Shreesha Raj <shreesharaj@gmail.com>
2018-06-20 23:20:26 +0530

    updated boilerplate

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CommentBox from './CommentBox';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<CommentBox />, document.getElementById('root'));
+
+registerServiceWorker();
