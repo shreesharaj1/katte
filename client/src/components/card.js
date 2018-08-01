c2e696f8b4c64d9778e0638a6102cbf6f833fc2b
Shreesha Raj <shreesha@SHREESH-MAC151629395.local>
2018-08-01 20:13:39 +0530

    manual draggable

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,85 +1,84 @@
 // Comment.js
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactMarkdown from 'react-markdown';
-import Draggable from 'react-draggable';
 
 class Card extends React.Component {
-getInitialState() {
-return {
-activeDrags: 0,
-deltaPosition: {
-x: 0, y: 0
-},
-controlledPosition: {
-x: -400, y: 200
-}
-};
-}
-handleDrag(e, ui) {
-const {x, y} = this.state.deltaPosition;
-this.setState({
-deltaPosition: {
-x: x + ui.deltaX,
-y: y + ui.deltaY,
-}
-});
-}
-onStart() {
-this.setState({activeDrags: ++this.state.activeDrags});
-}
-onStop() {
-this.setState({activeDrags: --this.state.activeDrags});
-}
-// For controlled component
-adjustXPos(e) {
-e.preventDefault();
-e.stopPropagation();
-const {x, y} = this.state.controlledPosition;
-this.setState({controlledPosition: {x: x - 10, y}});
-}
-adjustYPos(e) {
-e.preventDefault();
-e.stopPropagation();
-const {controlledPosition} = this.state;
-const {x, y} = controlledPosition;
-this.setState({controlledPosition: {x, y: y - 10}});
-}
-onControlledDrag(e, position) {
-const {x, y} = position;
-this.setState({controlledPosition: {x, y}});
-}
-onControlledDragStop(e, position) {
-this.onControlledDrag(e, position);
-this.onStop();
-}
-render() {
+  
+  state = {
+    tasks: [
+        {name:"Learn Angular",category:"complete", bgcolor: "yellow"},
+        {name:"React", category:"wip", bgcolor:"pink"},
+        {name:"Vue", category:"wip", bgcolor:"skyblue"}
+      ]
+  }
 
-return (
-  <Draggable
-      axis="x"
-      handle=".handle"
-      defaultPosition={{x: 0, y: 0}}
-      position={null}
-      grid={[25, 25]}
-      onStart={this.handleStart}
-      onDrag={this.handleDrag}
-      onStop={this.handleStop}>
-      <div className={`single-card ${this.props.suite}`}>
+onDragStart = (ev, id) => {
+    console.log('dragstart:',id);
+    ev.dataTransfer.setData("id", id);
+}
+
+onDragOver = (ev) => {
+    ev.preventDefault();
+}
+
+onDrop = (ev, cat) => {
+   let id = ev.dataTransfer.getData("id");
+   
+   let tasks = this.state.tasks.filter((task) => {
+       if (task.name == id) {
+           task.category = cat;
+       }
+       return task;
+   });
+
+   this.setState({
+       ...this.state,
+       tasks
+   });
+}
+
+  render() {
+    
+    var tasks = {
+      wip: [],
+      complete: []
+    }
+
+  this.state.tasks.forEach ((t) => {
+      tasks[t.category].push(
+          <div key={t.name} 
+              onDragStart = {(e) => this.onDragStart(e, t.name)}
+              draggable
+              className="draggable"
+              style = {{backgroundColor: t.bgcolor}}
+          >
+              {t.name}
+          </div>
+      );
+  });
+
+    return (
+      <div>
+      <div draggable className={`single-card ${this.props.suite}`}>
         <div className="textContent">
           <h3>{this.props.suite}</h3>
           <h3 className="text">{this.props.value}</h3>
         </div>
       </div>
-  </Draggable>
-);
+        
+        <div className="carpet" 
+            onDragOver={(e)=>this.onDragOver(e)}
+            onDrop={(e)=>this.onDrop(e, "complete")}>
+        </div>
+      </div>
+    );
 
-Comment.propTypes = {
-  author: PropTypes.string.isRequired,
-  children: PropTypes.string.isRequired,
-  timestamp: PropTypes.string.isRequired,
-};
-}
+  Comment.propTypes = {
+    author: PropTypes.string.isRequired,
+    children: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    };
+  }
 }
 
 export default Card;
