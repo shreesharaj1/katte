322abd9dd47f79f304540fe719c743eb7c0868ee
Shreesha Raj <shreesharaj@gmail.com>
2018-08-17 00:40:56 +0530

    dragg

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -3,72 +3,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Card extends React.Component {
-  
-  state = {
-    tasks: [
-        {name:"Learn Angular",category:"complete", bgcolor: "yellow"},
-        {name:"React", category:"wip", bgcolor:"pink"},
-        {name:"Vue", category:"wip", bgcolor:"skyblue"}
-      ]
-  }
-
-onDragStart = (ev, id) => {
-    console.log('dragstart:',id);
-    ev.dataTransfer.setData("id", id);
-}
-
-onDragOver = (ev) => {
-    ev.preventDefault();
-}
-
-onDrop = (ev, cat) => {
-   let id = ev.dataTransfer.getData("id");
-   
-   let tasks = this.state.tasks.filter((task) => {
-       if (task.name == id) {
-           task.category = cat;
-       }
-       return task;
-   });
-
-   this.setState({
-       ...this.state,
-       tasks
-   });
-}
 
   render() {
     
-    var tasks = {
-      wip: [],
-      complete: []
-    }
-
-  this.state.tasks.forEach ((t) => {
-      tasks[t.category].push(
-          <div key={t.name} 
-              onDragStart = {(e) => this.onDragStart(e, t.name)}
-              draggable
-              className="draggable"
-              style = {{backgroundColor: t.bgcolor}}
-          >
-              {t.name}
-          </div>
-      );
-  });
-
     return (
-      <div>
-      <div draggable className={`single-card ${this.props.suite}`}>
+      <div onDragStart={this.props.onDragStart} draggable className={`single-card ${this.props.suite}`}>
         <div className="textContent">
           <h3>{this.props.suite}</h3>
           <h3 className="text">{this.props.value}</h3>
-        </div>
-      </div>
-        
-        <div className="carpet" 
-            onDragOver={(e)=>this.onDragOver(e)}
-            onDrop={(e)=>this.onDrop(e, "complete")}>
         </div>
       </div>
     );
--- client/src/containers/game.js
+++ client/src/containers/game.js
@@ -1,19 +1,54 @@
 // Game.js
 import React, { Component } from 'react';
 import Card from '../components/card';
-import DATA from '../data';
 import '../css/app.css';
 
 class Game extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    const handCards = [<Card onDragStart={(e)=>this.onDragStart(e, "c1")} suite="clubs" value="10" id="c1" key="c1" />,
+            <Card onDragStart={(e)=>this.onDragStart(e, "c2")} suite="hearts" value="J" id="c2" key="c2" />]
+    this.state = { matCards: [], handCards };
   }
+
+  onDragStart = (ev, id) => {
+    console.log('dragstart:',id);
+    ev.dataTransfer.setData("id", id);
+  }
+
+  onDragOver = (ev) => {
+      ev.preventDefault();
+  }
+
+  onDrop = (ev, cat) => {
+     let id = ev.dataTransfer.getData("id");
+     let handCards = this.state.handCards;
+     let matCards = this.state.matCards;
+     handCards = handCards.filter((card) => {
+         if (card.props.id == id) {
+             matCards.push(card);
+             //handCards.remove(card);
+             return false;
+         }
+         return true;
+     });
+
+     this.setState({
+         handCards, matCards
+     });
+  }
+
   render() {
     return (
       <div className="container">
-          <Card suite="clubs" value="10" />
-          <Card suite="hearts" />
+          <div className="hand">
+            {this.state.handCards}
+           </div> 
+          <div className="carpet" 
+            onDragOver={(e)=>this.onDragOver(e)}
+            onDrop={(e)=>this.onDrop(e, "complete")}>
+            {this.state.matCards}
+          </div>
       </div>
     );
   }
