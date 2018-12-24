d6ced98258e47b677081fcace44fb79c5d4d76a3
Shreesha Raj <shreesha@SHREESH-MAC151629395.local>
2018-12-24 13:53:51 +0530

    draggable merge

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,21 +1,26 @@
 // Comment.js
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactMarkdown from 'react-markdown';
 
-const Card = props => (
-  <div className={`single-card ${props.suite}`}>
-    <div className="textContent">
-      <h3>{props.suite}</h3>
-      <h3 className="text">{props.value}</h3>
-     </div>
-  </div>
-);
+class Card extends React.Component {
 
-Comment.propTypes = {
-  author: PropTypes.string.isRequired,
-  children: PropTypes.string.isRequired,
-  timestamp: PropTypes.string.isRequired,
-};
+  render() {
+    
+    return (
+      <div onDragStart={this.props.onDragStart} draggable className={`single-card ${this.props.suite}`}>
+        <div className="textContent">
+          <h3>{this.props.suite}</h3>
+          <h3 className="text">{this.props.value}</h3>
+        </div>
+      </div>
+    );
+
+  Comment.propTypes = {
+    author: PropTypes.string.isRequired,
+    children: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    };
+  }
+}
 
 export default Card;
