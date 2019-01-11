092c57689618b81a88762d78301bca391f44bf63
Shreesha Raj <shreesha@SHREESH-MAC151629395.local>
2019-01-11 22:33:19 +0530

    hmm

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -7,9 +7,9 @@ class Card extends React.Component {
   render() {
     
     return (
-      <div onDragStart={this.props.onDragStart} draggable className={`single-card ${this.props.suite}`}>
-        <div className="suite-text">{this.props.suite}</div>
+      <div value={this.props.value} onDragStart={this.props.onDragStart} draggable className={`single-card ${this.props.suite}`}>
         <div className="value-text">{this.props.value}</div>
+        <div className="suite-text">{this.props.suite}</div>
       </div>
     );
 
