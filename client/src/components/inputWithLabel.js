6a595a1745bae88ec961a3882a33f7abe012d756
Shreesha Raj <shreesharaj@gmail.com>
2019-02-11 22:03:20 +0530

    class to fns

--- client/src/components/inputWithLabel.js
+++ client/src/components/inputWithLabel.js
@@ -1,27 +1,19 @@
 // inputWithLabel.js
-import React, { Component } from 'react';
-import Card from '../components/card';
-import Player from '../components/player';
+import React, { Component, useState } from 'react';
 import '../css/app.css';
 
-class InputWithLabel extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render (props) {
-      return (
-        <div classNamae="uk-margin">
-            <label className="uk-form-label" for={this.props.label}>{this.props.label}</label>
-            <div className="uk-form-controls">
-                <input className="uk-input" id={this.props.label} 
-                    type={this.props.type ? this.props.type : "text"} 
-                    placeholder={this.props.label}/>
-            </div>
-        </div>
-      )
-  }
+function InputWithLabel(props) {
+    console.log('sasddddddddddddddddd',useState);
+    return (
+      <div className="uk-margin">
+          <label className="uk-form-label" htmlFor={props.label}>{props.label}</label>
+          <div className="uk-form-controls">
+              <input className="uk-input uk-form-width-large" id={props.label} 
+                  type={props.type ? props.type : "text"} 
+                  placeholder={props.label}/>
+          </div>
+      </div>
+    )
 }
-
 
 export default InputWithLabel;
--- client/src/containers/signup.js
+++ client/src/containers/signup.js
@@ -3,24 +3,20 @@ import React, { Component } from 'react';
 import Input from '../components/inputWithLabel';
 import '../css/uikit.min.css';
 
-class Signup extends Component {
-  constructor(props) {
-    super(props);
-  }
+function Signup() {
 
-  render () {
-      return (
-        <div className="uk-container">
-            <div className="uk-grid uk-position-center">
-                <form className="uk-form-stacked">
-                    <Input label="Username" />
-                    <Input label="Email" />
-                    <Input label="Password" type="password" />
-                </form>
-            </div>    
+    return (
+    <div className="uk-container home-background">
+        <div className="uk-grid uk-position-center">
+            <form className="uk-form-stacked">
+                <Input label="Username" />
+                <Input label="Email" />
+                <Input label="Password" type="password" />
+                <button class="uk-button uk-button-primary uk-width-1-1">Submit</button>
+            </form>
         </div>    
-      )
-  }
+    </div>    
+    )
 }
 
 export default Signup;    
